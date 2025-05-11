296f05107371d4a63cd582eae429020612530c1c
JHenrique <henryque.pc.game@gmail.com>
2025-05-10 21:24:54 -0300

    Vamos Preparar a Base de Dados Para Autorização

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            // add the following columns
+            $table->string('role')->default('adnim');
+            $table->string('permissions')->default('adnim');
+
             $table->rememberToken();
             $table->timestamps();
         });
